ae8f789dfe35cfd4840222d5dcd9cd2986a8ea13
dmlbs <dmlb@users.noreply.github.com>
2025-04-05 17:10:17 -0600

    prefers-reduced-motion styles

--- src/components/Blockquote/Blockquote.tsx
+++ src/components/Blockquote/Blockquote.tsx
@@ -12,10 +12,10 @@ export default function Blockquote({ quote, person, position, company }: Props) 
         <blockquote className={styles.blockquote} data-testid="ncmp-blockquote">
             {quote}
             <cite>
-                <span className="name">{person}</span>
-                <span className="details">
-                    <span className="position">{position}</span>
-                    <span className="company">{company}</span>
+                <span className={styles.name}>{person}</span>
+                <span className={styles.details}>
+                    <span className={styles.position}>{position}</span>
+                    <span className={styles.company}>{company}</span>
                 </span>
             </cite>
         </blockquote>
