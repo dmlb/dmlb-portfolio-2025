65f46e97829afaee40de1a67303245198d9d76f3
dmlbs <dmlb@users.noreply.github.com>
2025-05-02 17:18:21 -0600

    add storybook controls for site settings

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,13 @@ import type { Preview } from "@storybook/react";
 import '../src/app/globals.css'
 
 const preview: Preview = {
+  decorators: [
+    (Story, context) => {
+      document.documentElement.setAttribute('data-mode', context.globals.theme);
+      document.documentElement.setAttribute('data-font', context.globals.font);
+      document.documentElement.setAttribute('data-motion', context.globals.animation);
+      return Story();
+    }],
   parameters: {
     backgrounds: { disable: true },
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
@@ -19,6 +26,43 @@ const preview: Preview = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      toolbar: {
+        title: 'Theme',
+        icon: 'circlehollow',
+        items: ['light', 'dark'],
+        dynamicTitle: true,
+      },
+    },
+    font: {
+      name: 'Font',
+      description: 'Global font for components',
+      toolbar: {
+        title: 'Font',
+        icon: 'document',
+        items: ['system', 'dyslexic'],
+        dynamicTitle: true,
+      }
+    },
+    animation: {
+      name: 'Reduce Motion',
+      description: 'Global reduce motion for components',
+      toolbar: {
+        title: 'Reduce Motion',
+        icon: 'play',
+        items: ['system', 'on', 'off'],
+        dynamicTitle: true,
+      },
+    }
+  },
+  initialGlobals: {
+    theme: 'dark',
+    font: 'system',
+    animation: 'system',
+  },
 };
 
 export default preview;
